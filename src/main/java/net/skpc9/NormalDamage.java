59f04e5a731442674a225156cef41c18b2a2aa97
SkpC9 <zhixinzong@gmail.com>
2023-07-24 23:58:04 +0800

    Revert "compile with java8"
    This reverts commit ada86cfae359f2e00cb7ca18835b8f31bcf33553.

--- src/main/java/net/skpc9/NormalDamage.java
+++ src/main/java/net/skpc9/NormalDamage.java
@@ -2,8 +2,12 @@ package net.skpc9;
 
 import net.fabricmc.api.ModInitializer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class NormalDamage implements ModInitializer {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger("normaldamage");
 
 	@Override
 	public void onInitialize() {
