11e716f75c146cbdcfb207000c9281c291a42e97
SkpC9 <zhixinzong@gmail.com>
2023-07-25 12:49:50 +0800

    forge 1.12

--- src/main/java/net/skpc9/NormalDamage.java
+++ src/main/java/net/skpc9/NormalDamage.java
@@ -4,10 +4,12 @@ package net.skpc9;
 import net.minecraftforge.fml.common.Mod;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(NormalDamage.MODID)
+@Mod(modid = NormalDamage.MODID, name = NormalDamage.NAME, version = NormalDamage.VERSION)
 public class NormalDamage
 {
     public static final String MODID = "normaldamage";
+    public static final String NAME = "Normal Damage";
+    public static final String VERSION = "1.0.0";
     public NormalDamage()
     {
 //        MinecraftForge.EVENT_BUS.register(this);
--- src/main/java/net/skpc9/mixin/DisableDamageScaleMixin.java
+++ src/main/java/net/skpc9/mixin/DisableDamageScaleMixin.java
@@ -1,12 +1,12 @@
 package net.skpc9.mixin;
 
 import net.minecraft.util.DamageSource;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.EntityPlayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(PlayerEntity.class)
+@Mixin(EntityPlayer.class)
 public class DisableDamageScaleMixin {
 	@Redirect(method = "attackEntityFrom", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/DamageSource;isDifficultyScaled()Z"))
 	private boolean injected(DamageSource source) {
